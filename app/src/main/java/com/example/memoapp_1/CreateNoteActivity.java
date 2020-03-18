e32244f543b81a8e14b0e4679ac78d95065c4830
ankit <ankitv397@gmail.com>
2020-03-19 00:42:02 +0530

    Prevented creation of empty note.

--- app/src/main/java/com/example/memoapp_1/CreateNoteActivity.java
+++ app/src/main/java/com/example/memoapp_1/CreateNoteActivity.java
@@ -6,12 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class CreateNoteActivity extends AppCompatActivity {
-    public final static String NOTE_RESULT_INTENT_KEY ="ADGJ";
+    public final static String NOTE_RESULT_INTENT_KEY = "ADGJ";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,11 +35,22 @@ public class CreateNoteActivity extends AppCompatActivity {
                 EditText tileText = findViewById(R.id.activity_create_note_title_edit_text);
                 EditText bodyText = findViewById(R.id.activity_create_note_body_edit_text);
 
-                Note saveNote = new Note(tileText.getText().toString(), bodyText.getText().toString(), null);
+                String titleContent = tileText.getText().toString();
+                String bodyContent = bodyText.getText().toString();
+
+                if (TextUtils.isEmpty(titleContent) && TextUtils.isEmpty(bodyContent)) {
+                    Toast.makeText(this,"Note can't be a blank note !!",Toast.LENGTH_SHORT).show();
+                    break;
+                }
+
+                Note saveNote = new Note(titleContent, bodyContent, null);
 
                 Intent intent = new Intent().putExtra(NOTE_RESULT_INTENT_KEY, saveNote);
-                setResult(Activity.RESULT_OK,intent);
+                setResult(Activity.RESULT_OK, intent);
                 finish();
+
+            default:
+                return false;
         }
 
 
